b79bebff9256d8f28be44918251e1d429145a28b
Gabriel Araujo <gabriel.r.araujo@accenture.com>
2024-10-22 16:27:00 -0300

    vai filhao

--- script.js
+++ script.js
@@ -1,75 +1,68 @@
-function buildHtmlContent() {
-    return `
-      <html>
-        <head>
-          <title>Tickets</title>
-          <style>
-            body,
-            html {
-              margin: 0;
-              padding: 0;
-            }
-            @media print {
-              @page {
-                margin: 0;
-              }
-              * {
-                color-adjust: exact;
-                -webkit-print-color-adjust: exact;
-                print-color-adjust: exact;
-              }
-              body {
-                width: 100%;
-                height: auto;
-                margin: 0;
-                padding: 0;
-                font-family: 'Helvetica', 'Roboto', 'Arial', sans-serif;
-                font-size: 14pt;
-              }
-              header, footer, nav, .no-print {
-                display: none;
-              }
-              .page {
-                page-break-after: always;
-              }
-            }
-          </style>
-        </head>
-        <body>
-          Tickets Content Printing
-          <img src="img.jpg"/>
-        </body>
-      </html>
-    `;
-}
+// function buildHtmlContent() {
+//     return `
+//       <html>
+//         <head>
+//           <title>Tickets</title>
+//           <style>
+//             body,
+//             html {
+//               margin: 0;
+//               padding: 0;
+//             }
+//             @media print {
+//               @page {
+//                 margin: 0;
+//               }
+//               * {
+//                 color-adjust: exact;
+//                 -webkit-print-color-adjust: exact;
+//                 print-color-adjust: exact;
+//               }
+//               body {
+//                 width: 100%;
+//                 height: auto;
+//                 margin: 0;
+//                 padding: 0;
+//                 font-family: 'Helvetica', 'Roboto', 'Arial', sans-serif;
+//                 font-size: 14pt;
+//               }
+//               header, footer, nav, .no-print {
+//                 display: none;
+//               }
+//               .page {
+//                 page-break-after: always;
+//               }
+//             }
+//           </style>
+//         </head>
+//         <body>
+//           Tickets Content Printing
+//           <img src="img.jpg"/>
+//         </body>
+//       </html>
+//     `;
+// }
 
-function buildIframeContent(ticketsContentHtml) {
-    const iframe = document.createElement('iframe');
-    // iframe.style.position = 'absolute';
-    // iframe.style.width = '0';
-    // iframe.style.height = '0';
-    // iframe.style.border = 'none';
+// function buildIframeContent(ticketsContentHtml) {
+//     const iframe = document.createElement('iframe');
 
-    document.body.appendChild(iframe);
+//     document.body.appendChild(iframe);
 
-    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-    iframeDocument.open();
-    iframeDocument.write(buildHtmlContent());
-    iframeDocument.close();
+//     const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+//     iframeDocument.open();
+//     iframeDocument.write(buildHtmlContent());
+//     iframeDocument.close();
 
-    return iframe;
-}
+//     return iframe;
+// }
 
 function printTickets() {
-    // const iframe = buildIframeContent();
-    // iframe.onload = () => {
-    //     iframe.contentWindow.focus();
-    //     iframe.contentWindow.print();
-    //     document.body.removeChild(iframe);
-    //     window.focus();
-    // };
     window.print();
 }
+
+setInterval( () => {
+    console.log(Date.now());
+});
 
 const button = document.getElementById('print');
 button.addEventListener('click', printTickets);
